98670f87829a709325cde74ae0d728eff2455751
Md Shovo <shovom677@gmail.com>
2024-05-31 16:33:17 +0600

    Create Exponsive Component

---
+++ src/ExponsiveItem/Expons.js
@@ -0,0 +1,20 @@
+import './Expons.css'
+
+function Expons(){
+    return(
+        <div className='expons'>
+            <h1>Lets Get Start</h1>
+            <div className='expons-item'>
+                <div>
+                    <h1>March 12th 2021</h1>
+                </div>
+                <div className='insurance'>
+                       <h2>Care insurance</h2>
+                        <div className='button'>$200</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Expons;
